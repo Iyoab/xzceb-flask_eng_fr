5f4b57c1b33b8ceb8d0ea030d7ef39c1e8243963
Iyoab <iyoabseid135@gmail.com>
2023-03-21 12:34:51 -0400

    Final revised changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,15 +1,22 @@
 import unittest
 from translator import english_to_french, french_to_english
-class TestTranslator(unittest.TestCase):
-    def test1_english_to_french(self):
-        self.assertEqual(english_to_french("Yes"),"Oui")
-    def test2_french_to_english(self):
-        self.assertEqual(french_to_english("Oui"),"Yes")        
-    def test_null_input_french_to_english(self):
-        self.assertIsNone(None)
-    def test_null_input_english_to_french(self):
-        self.assertIsNone(None)
-    def test1_french_to_english(self):
-        self.assertEqual(french_to_english("Bonjour"),"Hello")
-    def test2_english_to_french(self):
-        self.assertEqual(english_to_french("Hello"),"Bonjour")
+
+
+class Test_English_Translator(unittest.TestCase):
+
+    def test1(self):
+        self.assertNotEqual(english_to_french('None'), '')
+
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+
+class Test_French_Translator(unittest.TestCase):
+
+    def test1(self):
+        self.assertNotEqual(french_to_english('None'), '')
+
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,13 +2,13 @@ import json
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
@@ -17,19 +17,26 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(url)
 
-translation = language_translator.translate(
-    text='Bonjour',
-    model_id='fr-en').get_result()
-print(json.dumps(translation, indent=2, ensure_ascii=False))
+
 def english_to_french(english_text):
-    translation = language_translator.translate(text=english_text,
+    """
+    translating from english to french
+    """
+    translation_french = language_translator.translate(
+        text=english_text,
         model_id='en-fr').get_result()
-    french_text = translation['translations'][0]['translation']
-    # print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+    french_text = translation_french['translations'][0]['translation']
+
     return french_text
 
+
 def french_to_english(french_text):
-    translation = language_translator.translate(text=french_text,
+    translation_english = language_translator.translate(
+        text=french_text,
         model_id='fr-en').get_result()
-    english_text = translation['translations'][0]['translation']
+
+    english_text = translation_english['translations'][0]['translation']
+
     return english_text
+# print("The function translated the given word to: " + frenchtoEnglish("Je vous"))
